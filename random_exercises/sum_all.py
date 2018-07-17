bb56340160b9c93e38a496834f7dc33ee83f2336
sagar <sagarmaharjan31@gmail.com>
2018-07-17 20:37:03 +0545

    sum_all.py file modified.

--- random_exercises/sum_all.py
+++ random_exercises/sum_all.py
@@ -4,7 +4,7 @@ def sum_all(*n):
 
 
 def main():
-    print("Sum of the number{} in a tuple is: {}".format(number, sum_all(1, 2, 3, 4, 5, 6, 7)))
+    print("Sum of the given numbers{} in a tuple is: {}".format(number, sum_all(1, 2, 3, 4, 5, 6, 7)))
 
 
 if __name__ == "__main__":
