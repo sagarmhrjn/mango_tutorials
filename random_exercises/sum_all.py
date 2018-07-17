811ad146e7aa98e59c92afb83be93bc17b8f4cb5
sagar <sagarmaharjan31@gmail.com>
2018-07-17 20:58:38 +0545

    sum_all.py is modified

--- random_exercises/sum_all.py
+++ random_exercises/sum_all.py
@@ -1,11 +1,18 @@
 number = (1, 2, 3, 4, 5, 6, 7)
+
+
 def sum_all(*n):
     return(sum(n))
 
+def sum_all1(numbers):
+    result = 0
+    for i in numbers:
+        result = result + i
+    return result
 
 def main():
     print("Sum of the given numbers{} in a tuple is: {}".format(number, sum_all(1, 2, 3, 4, 5, 6, 7)))
-
+    print("Sum of numbers using for loop: %d" % sum_all1(number))
 
 if __name__ == "__main__":
     main()
