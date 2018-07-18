a24c205e756012983202694555f66ed7fc6770a9
sagar <sagarmaharjan31@gmail.com>
2018-07-18 12:23:14 +0545

    program is modified with reduce func

--- random_exercises/sum_all.py
+++ random_exercises/sum_all.py
@@ -1,5 +1,7 @@
-number = (1, 2, 3, 4, 5, 6, 7)
+from functools import reduce
 
+#list of numbers in a tuple:
+number = (1, 2, 3, 4, 5, 6, 7)
 
 def sum_all(*n):
     return(sum(n))
@@ -13,6 +15,7 @@ def sum_all1(numbers):
 def main():
     print("Sum of the given numbers{} in a tuple is: {}".format(number, sum_all(1, 2, 3, 4, 5, 6, 7)))
     print("Sum of numbers using for loop: %d" % sum_all1(number))
-
+    print("Sum of numbers in a tuple using reduce function: " + str(reduce((lambda x, y : x + y), number)))
+    
 if __name__ == "__main__":
     main()
